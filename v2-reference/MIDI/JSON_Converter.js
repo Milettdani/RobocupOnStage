aeacc2b4b3505723efa390d88574791ad0d47bab
Divdav419 <47518541+Divdav419@users.noreply.github.com>
2021-06-19 16:29:13 +0200

    edited JSON_Converter

--- v2-reference/MIDI/JSON_Converter.js
+++ v2-reference/MIDI/JSON_Converter.js
@@ -8,82 +8,71 @@
 //	midi: 60 -> C4
 
 Object.size = function(obj) {
-		var size = 0, key;
-		for (key in obj) {
-				if (obj.hasOwnProperty(key)) size++;
-		}
-		return size;
+	var size = 0, key;
+	for (key in obj) {
+			if (obj.hasOwnProperty(key)) size++;
+	}
+	return size;
 };
 
 function Copy() {
-	var copyText = document.getElementById("resultTextarea");
-	copyText.select();
-	copyText.setSelectionRange(0, 99999);
-	document.execCommand("copy");
+var copyText = document.getElementById("resultTextarea");
+copyText.select();
+copyText.setSelectionRange(0, 99999);
+document.execCommand("copy");
+}
+
+function WriteToFile(arr)
+{
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var s = fso.CreateTextFile("C:\\drumsRead.txt", true);
+s.WriteLine(arr);
+s.Close();
 }
 
 var final = "";
 const input = document.querySelector('input[type="file"]');
 input.addEventListener('change', function (e) {
-	//console.log(input.files)
-	const reader = new FileReader()
-	reader.onload = function() {
-		var parsedMusic = JSON.parse(reader.result)
-		console.log(parsedMusic)
+//console.log(input.files)
+const reader = new FileReader()
+reader.onload = function() {
+	var parsedMusic = JSON.parse(reader.result)
+	console.log(parsedMusic)
 
-		var t = [];
-		var d = true;
-		var p = true;
-		for (var i = 0; i < Object.size(parsedMusic.tracks); i++) {
-			//Creat arrays
-			if (Object.size(parsedMusic.tracks[i].notes) > 0) {
-				if (JSON.stringify(parsedMusic.tracks[i]).includes('isPercussion') && parsedMusic.tracks[i].isPercussion) {
-					if (d == true) {
-						t[i] = "const double d" + "[" + Object.size(parsedMusic.tracks[i].notes) * 2 + "] = {"
-						for (var j = 0; j < Object.size(parsedMusic.tracks[i].notes); j++) {
-							if (j > 0) t[i] += ", "
-							t[i] += parsedMusic.tracks[i].notes[j].midi + ", " + parsedMusic.tracks[i].notes[j].time.toFixed(2);
-						}
-						/*console.log("int t" + i + "[" + Object.size(parsedMusic.tracks[i].notes) + "] = ")
-						//Creat elements
-						var toNext = 0
-
-						for (var j = 0; j < Object.size(parsedMusic.tracks[i].notes); j++) {
-							if (j > 0) {
-                                                                toNext = parsedMusic.tracks[i].notes[j].time - parsedMusic.tracks[i].notes[j-1].time
-                                                        } else {
-								toNext = parsedMusic.tracks[i].notes[j].time
-							}
-
-
-							if (j > 0) {
-								t[i] += ", "
-							}
-							t[i] += parsedMusic.tracks[i].notes[j].midi + ", " + toNext.toFixed(2);
-							//console.log(parsedMusic.tracks[i].notes[j].midi + ", " + parsedMusic.tracks[i].notes[j].time + ", " + parsedMusic.tracks[i].notes[j].duration)
-						}*/
-						d = false
+	var d = true;
+	var p = true;
+	for (var i = 0; i < Object.size(parsedMusic.tracks); i++) {
+		//Creat arrays
+		if (Object.size(parsedMusic.tracks[i].notes) > 0) {
+			var t = "";
+			if (JSON.stringify(parsedMusic.tracks[i]).includes('isPercussion') && parsedMusic.tracks[i].isPercussion) {
+				if (d == true) {
+					t = "const double d" + "[" + Object.size(parsedMusic.tracks[i].notes) * 2 + "] = {"
+					for (var j = 0; j < Object.size(parsedMusic.tracks[i].notes); j++) {
+						if (j > 0) t += ", "
+						t += parsedMusic.tracks[i].notes[j].midi + ", " + parsedMusic.tracks[i].notes[j].time.toFixed(2);
 					}
-				} else {
-					if (p == true) {
-						t[i] = "const double p" + "[" + Object.size(parsedMusic.tracks[i].notes) * 3 + "] = {"
-						for (var j = 0; j < Object.size(parsedMusic.tracks[i].notes); j++) {
-							if (j > 0) t[i] += ", "
-							t[i] += parsedMusic.tracks[i].notes[j].midi + ", " + parsedMusic.tracks[i].notes[j].time.toFixed(2) + ", " + parsedMusic.tracks[i].notes[j].duration.toFixed(2);
-						}
-						p = false
-					}
+					d = false;
+					//WriteToFile(t + "};");
 				}
-					t[i] += "};"
-					final += t[i] + "\n"
-					//console.log(final)
-		}
-		}
-		//document.getElementById("resultTextarea").value = final;
+			} else {
+				if (p == true) {
+					t = "const double p" + "[" + Object.size(parsedMusic.tracks[i].notes) * 3 + "] = {"
+					for (var j = 0; j < Object.size(parsedMusic.tracks[i].notes); j++) {
+						if (j > 0) t += ", "
+						t += parsedMusic.tracks[i].notes[j].midi + ", " + parsedMusic.tracks[i].notes[j].time.toFixed(2) + ", " + parsedMusic.tracks[i].notes[j].duration.toFixed(2);
+					}
+					p = false
+				}
+			}
+			final += t + "};\n"
 	}
-	reader.readAsText(input.files[0])
+	}
+	//document.getElementById("resultTextarea").value = final;
+}
+reader.readAsText(input.files[0])
 }, false);
 
 function onSubmit() {
-	document.getElementById("resultTextarea").value = final;
+document.getElementById("resultTextarea").value = final;
 }
