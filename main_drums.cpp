84aa54c3158d83e7465e2bf5c81bf4f30620dcea
Divdav419 <47518541+Divdav419@users.noreply.github.com>
2021-06-19 23:28:09 +0200

    Update main_drums.cpp

--- main_drums.cpp
+++ main_drums.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 
 // Time between elements of final array (in seconds)
-const double arrayTime = 0.5;
+const double arrayTime = 0.25;
 // Input array = {midi value, start time}
+
+
 const double d[20] = {36, 0.00, 36, 1.00, 36, 2.00, 38, 3.00, 36, 4.00, 36, 5.00, 36, 5.50, 36, 6.00, 36, 7.00, 38, 7.00};
 
 const int midi_length = 7;
@@ -29,13 +31,41 @@ void editFile(string str_replace)		//NEEDS EDITING
 	out_file << str;
 }
 
+void writeInteract(int arr[], int as)
+{
+	string drumsw = to_string(as) + "\n";
+	string pianow = "";
+	for (int i=0; i<as; i++) {
+		drumsw += to_string(arr[i]);
+		if (arr[i] == 136) {
+			pianow += i*arrayTime;
+			if (i != as-1) pianow += ", ";
+		}
+		if (i != as-1) drumsw += ", ";
+	}
+	drumsw += "\n" + to_string(arrayTime);
+	
+	//Drums
+	fstream dout;
+	dout.open("test.txt", ios::out | ios::trunc);
+	dout << drumsw;
+	dout.close();
+	
+	//Piano
+	fstream pout;
+	pout.open("test.txt", ios::out | ios::trunc);
+	pout << pianow;
+	pout.close();
+}
+
 int main()
 {
 	//Generate empty array
 	int size = (d[sizeof(d)/sizeof(double)-1])/arrayTime +1; //length of final array: length of track / arrayTime
     
-    string f = "\nconst int d[" + to_string(size+1) + "] = {";
+    	string f = "\nconst int d[" + to_string(size+1) + "] = {";
 	string uf;
+	int of[size];
 	for (int t=0;t<size;t++) { // t+1th atTime in g
 		uf = "10000000";
 		// loop through d
@@ -49,10 +79,12 @@ int main()
 			}
 			// Is true if at time t p[i-1] is held down at time t
 		}
-		uf = to_string(stoi(uf, 0, 2)) + ", ";
+		of[t] = stoi(uf, 0, 2);
+		uf = to_string(of[t]) + ", ";
 		f += uf;
 	}
 	f += "128};\nconst int arrSize = " + to_string(size+1) + ";\nconst double arrayTime = " + to_string(arrayTime) + ";\n\n";
 	editFile(f);
+	writeInteract(of, size);
 	//cout << f;
 }
