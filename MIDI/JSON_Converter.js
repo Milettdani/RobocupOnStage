e6e84e6c908204b6ad4cdfe1bb3ea9f104d65bd1
Divdav419 <47518541+Divdav419@users.noreply.github.com>
2021-06-22 12:38:29 +0200

    Added Write to PianoRead.txt

--- MIDI/JSON_Converter.js
+++ MIDI/JSON_Converter.js
@@ -38,6 +38,7 @@ reader.onload = function() {
 		if (Object.size(parsedMusic.tracks[i].notes) > 0) {
 			var t = "";
 			var fw = "";
+			var pw = "";
 			if (JSON.stringify(parsedMusic.tracks[i]).includes('isPercussion') && parsedMusic.tracks[i].isPercussion) {
 				if (d == true) {
 					t = "const double d" + "[" + Object.size(parsedMusic.tracks[i].notes) * 2 + "] = {"
@@ -57,15 +58,20 @@ reader.onload = function() {
 			} else {
 				if (p == true) {
 					t = "const double p" + "[" + Object.size(parsedMusic.tracks[i].notes) * 3 + "] = {"
+					pw = parsedMusic.tracks[i].notes[0].midi + Object.size(parsedMusic.tracks[i].notes) * 3;
 					for (var j = 0; j < Object.size(parsedMusic.tracks[i].notes); j++) {
-						if (j > 0) t += ", "
+						if (j > 0) {
+							t += ", "
+							pw += "\n"
+						}
 						t += parsedMusic.tracks[i].notes[j].midi + ", " + parsedMusic.tracks[i].notes[j].time.toFixed(2) + ", " + parsedMusic.tracks[i].notes[j].duration.toFixed(2);
+						pw += parsedMusic.tracks[i].notes[j].midi + "\n" + parsedMusic.tracks[i].notes[j].time.toFixed(2) + "\n" + parsedMusic.tracks[i].notes[j].duration.toFixed(2);
 					}
 					p = false
 				}
 			}
 			final += t + "};\n"
-			if (i == Object.size(parsedMusic.tracks)-1) final += '\n\n' + fw;
+			if (i == Object.size(parsedMusic.tracks)-1) final += '\n\n' + fw + "\n" + pw;
 	}
 	}
 	//document.getElementById("resultTextarea").value = final;
