88fc779cbc9773ab1fc8f301a0fe2cad5d6d6d54
danielbacsur <85509485+danielbacsur@users.noreply.github.com>
2021-06-25 20:19:38 +0200

    Add files via upload

---
+++ FinalFinal/Controller/Main.py
@@ -0,0 +1,22 @@
+import Controller
+
+if __name__ == '__main__':
+    controller = Controller.Controller()
+    try:
+        controller.connect()
+        input('Press ENTER to start...')
+        controller.start()
+        while controller.isPlaying:
+            controller.main(controller.start_time)
+        input("special...")
+        controller.special()
+        
+        input("start interact")
+        controller.write('X', b'I')
+        controller.stop()
+        
+    except KeyboardInterrupt:
+        controller.cancel()
+    finally:
+        
+        controller.disconnect()
